bef0a574bb4c3be4fb50333cee4ea09ad534d358
Daniel Antwi <38796291+dantwi@users.noreply.github.com>
2019-11-28 19:44:02 -0500

    format App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import About from "./About.jsx";
 
@@ -7,17 +6,25 @@ function App() {
   return (
     <div>
       <ul>
-        <li className ="logo">
-        <svg width="55" height="55" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M14.0829 10.0268L43.279 22.7459L15.4226 41.845L14.0829 10.0268Z" fill="#FF6347"/>
-</svg>
-
+        <li className="logo">
+          <svg
+            width="55"
+            height="55"
+            viewBox="0 0 50 50"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M14.0829 10.0268L43.279 22.7459L15.4226 41.845L14.0829 10.0268Z"
+              fill="#FF6347"
+            />
+          </svg>
         </li>
         <li>
           <a href="news.asp">Documentation</a>
         </li>
         <li>
-          <a href="contact.asp">About</a>
+          <a href="./About.jsx">About</a>
         </li>
         <li>
           <a href="about.asp">Home</a>
